cdf74e0f9a29796b16d1873992f5242f5b608a4d
RakshithaBharath <raksh1_reddy@outlook.com>
2024-09-30 23:12:10 +0530

    Adding Toggle button

--- amazon/Java_script/index.js
+++ amazon/Java_script/index.js
@@ -4,5 +4,10 @@ const userName = sessionStorage.getItem("userName");
         if (userName) {
             document.getElementById("profile_name").innerHTML = `Welcome <br> ${userName}`;
         } else {
-            document.getElementById("profile_name").innerHTML = "Welcome Guest";
-        }
+            document.getElementById("profile_name").innerHTML = "Welcome <br> Guest";
+}
+        
+    function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
